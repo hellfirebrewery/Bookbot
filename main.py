8559eb47101e15cc343fdfa82d2caf01defd608c
hellfirebrewery <hellfirebrewery@gmail.com>
2025-04-30 12:25:17 -0600

    3 commit move def

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+def main():
+    filename = "books/frankenstein.txt"
+    output = get_book_text(filename)
+    from stats import word_count
+    count = word_count(output)
+    print(f"{count} words found in the document")
+
 def get_book_text(filename):
     with open(filename) as f:
     # do something with f (the file) here
    
-   # f is a file object
-        file_contents = f.read()
-    return file_contents
+    # f is a file object
+        return f.read()
 
-
-def main():
-    filename = "books/frankenstein.txt"
-    output = get_book_text(filename)
-    print(output)
 
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def word_count(output):
+    count = output.split()
+    num = len(count)
+    return num
