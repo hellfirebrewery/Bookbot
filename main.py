632480ba4c14f8c2292603573f058ae5df2ec7bd
hellfirebrewery <hellfirebrewery@gmail.com>
2025-04-30 16:50:20 -0600

    5 commit

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 def main():
     filename = "books/frankenstein.txt"
     output = get_book_text(filename)
+    
     from stats import word_count
     count = word_count(output)
-    print(f"{count} words found in the document")
+    #print(f"{count} words found in the document")
+
+    from stats import character_count
+    char = character_count(output)
+    print(f"{char}")
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
+
 
 def get_book_text(filename):
     with open(filename) as f:
--- stats.py
+++ stats.py
@@ -2,3 +2,10 @@ def word_count(output):
     count = output.split()
     num = len(count)
     return num
+
+def character_count(output):
+    char_count = {}
+    out1 = output.lower()
+    for char in out1:
+        char_count[char] = char_count.get(char, 0) + 1
+    return char_count
